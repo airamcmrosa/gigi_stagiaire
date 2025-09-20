cdfdea28a42a325be3c204d71667ea39e4d4d9ff
Maria <maria.rosa@thoughtworks.com>
2025-09-19 22:35:16 -0300

    git commit -m "fix models "

--- lib/domain/questions.dart
+++ lib/domain/questions.dart
@@ -1,14 +1,14 @@
 import 'package:equatable/equatable.dart';
 import 'package:gigi_stagiaire/domain/personagem.dart';
 
-class Questions extends Equatable {
+class Question extends Equatable {
   final String pergunta;
   final String perguntaTraduzida;
   final String respostaCorreta;
   List<String>? respostasErradas;
   final Personagem personagem;
 
-  Questions({
+  Question({
     required this.pergunta,
     required this.perguntaTraduzida,
     required this.respostaCorreta,
@@ -18,3 +18,4 @@ class Questions extends Equatable {
   @override
   List<Object?> get props => [pergunta, perguntaTraduzida, respostaCorreta];
 }
+//Criar um json com as perguntas de teste, transformar esse objeto json em um objeto entity e entao transformar num objeto question
