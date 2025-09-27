490e7240a2db1f25632de51775a80a060e85445b
AndreVictordmr <41845560+AndreVictordmr@users.noreply.github.com>
2025-09-27 14:09:55 -0300

    nao sei se esta correto

--- lib/data/repository/datasource/get_data.dart
+++ lib/data/repository/datasource/get_data.dart
@@ -1,8 +1,23 @@
-import 'asset/json/perguntas.json';
+import 'domain/personagem.dart';
 import 'domain/questions.dart';
 import 'dart:convert';
-//Não sei se esta correto essa parte de import
+//Não sei se esta correto essa parte de import 
 
-class QuestionData {
-    
+class GetDado {
+	final String pergunta;
+    final String resposta;
+    final String traducao;
+
+    factory GetDado.fromJson(Map<String, dynamic> json){
+        return New Questions()
+    }
+
+ 	Future<List<Questions>> carregandoPerguntas() {
+        //aqui eu leio o arquivo
+		String load = await rootBundle.loadString('asset/json/perguntas.json');
+		//aqui eu decodifico ele
+        final List<dynamic> dados =json.decode(load);
+        //e aqui passo os dados pra Questions
+        return dados.map((e)=>GetDado.fromJson(e)).toList();
+    }
 }
